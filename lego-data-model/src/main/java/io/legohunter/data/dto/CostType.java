5bd62fe1356709400cd4658ca43931eee1241e71
tvatt <github@vattima.com>
2026-07-27 12:05:02 -0400

    Use cost type code for cost type equality

--- lego-data-model/src/main/java/io/legohunter/data/dto/CostType.java
+++ lego-data-model/src/main/java/io/legohunter/data/dto/CostType.java
@@ -2,10 +2,13 @@ package io.legohunter.data.dto;
 
 import lombok.Builder;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 @Data
 @Builder
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class CostType {
+    @EqualsAndHashCode.Include
     private String costTypeCode;
     private String costTypeName;
     private String costTypeDescription;
